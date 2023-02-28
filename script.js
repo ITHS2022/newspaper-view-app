99a232d86dd13a282592cf0789fcce1cace26a6b
ITHS2022 <114249345+ITHS2022@users.noreply.github.com>
2023-02-28 21:30:47 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ function get() {// Get リクエスト
         .then(data => {
             let result = document.getElementById('view_links');
             result.innerHTML = ""; //resultのHTMLを一度リセット
+            
             for (var i = 0; i < data.main.length; i++){
                     const number = i+1; //新聞連番表示用 jsonが0から始まるので+1
-                    result.insertAdjacentHTML('beforeend', `<div class="result"><div class="date">${data.main[i][0]}年${data.main[i][1]}月${data.main[i][2]}日発行</div><div class="title">生徒会新聞${number}</div><a href="/data/2022/${number}.pdf"></a></div>`);
+                    result.insertAdjacentHTML('beforeend', `<div class="result"><div class="date">${data.main[i][0]}年${data.main[i][1]}月${data.main[i][2]}日発行</div><div class="title">生徒会新聞${number}</div><a href="/data/2022/${number}.pdf" target="_blank"></a></div>`);
                 }
-            })
+            }
+            )
         })
     }
